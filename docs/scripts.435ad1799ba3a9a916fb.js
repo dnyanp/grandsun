fabee5f686b772de535512885a57b880a5ee66ab
Dnyaneshwar Wagh <dnyaneshwar.wagh@techniatranscat.com>
2020-04-30 22:23:15 +0530

    depedencies updation

--- docs/scripts.435ad1799ba3a9a916fb.js
+++
@@ -1,1 +0,0 @@
-$(document).ready((function(){$("nav").toggleClass("border-bottom shadow-sm scrolled",$(this).scrollTop()>30),$("p.backTop").toggleClass("show",$(this).scrollTop()>300)})),$(window).scroll((function(){$("nav").toggleClass("border-bottom shadow-sm scrolled",$(this).scrollTop()>30),$("p.backTop").toggleClass("show",$(this).scrollTop()>300)}));
